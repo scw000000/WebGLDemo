cdefc7149106ec847bab3e85c8ff9c75f6cf0532
scw000000 <scw000000@gmail.com>
2016-09-14 22:27:41 -0400

    implemented graph animation

--- Lab1/Lab1/scripts/code05.js
+++ Lab1/Lab1/scripts/code05.js
@@ -2,6 +2,7 @@
 var gl;
 var shaderProgram;
 var draw_type = 2;
+var readyToDraw = false;
 
 //////////// Init OpenGL Context etc. ///////////////
 
@@ -25,17 +26,25 @@ var squareVertexPositionBuffer;
 var squareVertexColorBuffer;
 var squareVertexIndexBuffer;
 
-var vertices = [];
+var barVertices = [];
+var targetVertices = [];
 var indices = [];
 var num_vertices;
 var num_indices;
+// the highest bar is max bar, which height is 2 - 2 * v_margin, start from -1 + v_margin
+var v_margin = 0.25;
+var h_margin = 0.1;
+var num_bars = 0;
 
 function createBarVertices( avgs )
    {
-   vertices = [];
-   indices = [];
+   readyToDraw = false;
 
-   var num_bars = avgs.length;
+   barVertices = [];
+   indices = [];
+   targetVertices = [];
+
+   num_bars = avgs.length;
    num_vertices = num_bars * 4;
    num_indices = num_bars * 6;
 
@@ -52,8 +61,7 @@ function createBarVertices( avgs )
       }
    width = max - min;
    console.log( "min = " + min + " max = " + max );
-   // the highest bar is max bar, which height is 2 - 2 * v_margin, start from -1 + v_margin
-   var v_margin = 0.25;
+   
    // the total with of canvas is 2, from -1 to +1
    // each bar is 2h in width, and distance between each bar is h, so each bar take 3h width
    // the x coordinates starts from -1 + h
@@ -62,13 +70,20 @@ function createBarVertices( avgs )
       {
       // x, y, z coordinates
       // vertex 0, down left
-      vertices.push( -1 + ( 3 * i + 1 ) * h ); vertices.push( -1 + v_margin ); vertices.push( 0.0 );
+      barVertices.push( -1 + ( 3 * i + 1 ) * h ); barVertices.push( -1 + v_margin ); barVertices.push( 0.0 );
+      targetVertices.push( -1 + ( 3 * i + 1 ) * h ); targetVertices.push( -1 + v_margin ); targetVertices.push( 0.0 );
+      
       // vertex 1, donw right
-      vertices.push( -1 + ( 3 * i + 3 ) * h ); vertices.push( -1 + v_margin ); vertices.push( 0.0 );
+      barVertices.push( -1 + ( 3 * i + 3 ) * h ); barVertices.push( -1 + v_margin ); barVertices.push( 0.0 );
+      targetVertices.push( -1 + ( 3 * i + 3 ) * h ); targetVertices.push( -1 + v_margin ); targetVertices.push( 0.0 );
+
       // vertex 2, up right
-      vertices.push( -1 + ( 3 * i + 3 ) * h ); vertices.push( -1 + v_margin + ( 2 - 2 * v_margin ) * ( avgs[i] - min ) / width ); vertices.push( 0.0 );
+      barVertices.push( -1 + ( 3 * i + 3 ) * h ); barVertices.push( -1 + v_margin ); barVertices.push( 0.0 );
+      targetVertices.push( -1 + ( 3 * i + 3 ) * h ); targetVertices.push( -1 + v_margin + ( 2 - 2 * v_margin ) * ( avgs[i] - min ) / width ); targetVertices.push( 0.0 );
+      
       // vertex 3, up left
-      vertices.push( -1 + ( 3 * i + 1 ) * h ); vertices.push( -1 + v_margin + ( 2 - 2 * v_margin ) * ( avgs[i] - min ) / width ); vertices.push( 0.0 );
+      barVertices.push( -1 + ( 3 * i + 1 ) * h ); barVertices.push( -1 + v_margin ); barVertices.push( 0.0 );
+      targetVertices.push( -1 + ( 3 * i + 1 ) * h ); targetVertices.push( -1 + v_margin + ( 2 - 2 * v_margin ) * ( avgs[i] - min ) / width ); targetVertices.push( 0.0 );
 
       indices.push( 0 + 4 * i ); indices.push( 1 + 4 * i ); indices.push( 2 + 4 * i );
       indices.push( 0 + 4 * i ); indices.push( 2 + 4 * i ); indices.push( 3 + 4 * i );
@@ -76,16 +91,21 @@ function createBarVertices( avgs )
 
    initBuffers();
 
-   drawScene();
+   readyToDraw = true;
    }
 
 ////////////////    Initialize VBO  ////////////////////////
+var leftShift = 0.05;
+var edgeLineVertices = [ leftShift + -1 + h_margin, -1 + v_margin, 0.0, 
+                          1 - h_margin, -1 + v_margin, 0.0, 
+                         leftShift + -1 + h_margin, -1 + v_margin, 0.0,
+                         leftShift + -1 + h_margin, 1 - v_margin, 0.0 ];
 
 function initBuffers()
    {
    squareVertexPositionBuffer = gl.createBuffer();
    gl.bindBuffer( gl.ARRAY_BUFFER, squareVertexPositionBuffer );
-   gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( vertices ), gl.STATIC_DRAW );
+   gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( barVertices ), gl.STATIC_DRAW );
    squareVertexPositionBuffer.itemSize = 3;
    squareVertexPositionBuffer.numItems = num_vertices;
 
@@ -94,6 +114,12 @@ function initBuffers()
    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array( indices ), gl.STATIC_DRAW );
    squareVertexIndexBuffer.itemsize = 1;
    squareVertexIndexBuffer.numItems = num_indices;
+
+   lineVertexBuffer = gl.createBuffer();
+   gl.bindBuffer( gl.ARRAY_BUFFER, lineVertexBuffer );
+   gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( edgeLineVertices ), gl.STATIC_DRAW );
+   lineVertexBuffer.itemSize = 3;
+   lineVertexBuffer.numItems = 4;
    }
 
 window.requestAnimFrame = (function() {
@@ -107,11 +133,29 @@ window.requestAnimFrame = (function() {
          };
 })();
 
+var lerpFactor = 0.94;
+var inverseLerpFactor = 1.0 - lerpFactor;
+
+function updateBarVertices()
+   {   
+   for( var i = 0; i < num_bars; ++i )
+      {
+      if( barVertices[ 12 * i + 7 ] != targetVertices[ 12 * i + 7 ] )
+         {
+         barVertices[ 12 * i + 7 ] = inverseLerpFactor * targetVertices[ 12 * i + 7 ] + lerpFactor * barVertices[ 12 * i + 7 ];
+         barVertices[ 12 * i + 10 ] = inverseLerpFactor * targetVertices[ 12 * i + 10 ] + lerpFactor * barVertices[ 12 * i + 10 ];
+         }
+      }
+   gl.bindBuffer( gl.ARRAY_BUFFER, squareVertexPositionBuffer );
+   gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( barVertices ), gl.STATIC_DRAW );  
+   }
+
 function onUpdate() 
    {
    requestAnimFrame( onUpdate );
-   if( has_data )
+   if( readyToDraw )
       {
+      updateBarVertices();
       drawScene();
       }
    }
@@ -120,7 +164,6 @@ function onUpdate()
 
 function drawScene()
    {
-   console.log( "draw" );
    gl.viewport( 0, 0, gl.viewportWidth, gl.viewportHeight );
    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
@@ -132,6 +175,9 @@ function drawScene()
 
    gl.drawElements( gl.TRIANGLES, num_indices, gl.UNSIGNED_SHORT, 0 );
 
+   gl.bindBuffer( gl.ARRAY_BUFFER, lineVertexBuffer );
+   gl.vertexAttribPointer( shaderProgram.vertexPositionAttribute, lineVertexBuffer.itemSize, gl.FLOAT, false, 0, 0 );
+   gl.drawArrays( gl.LINES, 0, lineVertexBuffer.numItems ); 
    }
 
 
@@ -147,13 +193,10 @@ function webGLStart()
    gl.enableVertexAttribArray( shaderProgram.vertexPositionAttribute );
 
    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-
+   onUpdate();
    }
 
 function BG( red, green, blue )
    {
-
    gl.clearColor( red, green, blue, 1.0 );
-   drawScene();
-
    }
